2a2ac11298193d62301a85c89ef735384d55db62
GiBaek Yu <zxdfg114@gmail.com>
2025-04-12 17:59:10 +0900

    수정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/zxdfg114/Simulator/", // <- 꼭 수정!!
+  base: "/Simulator/", // <- 꼭 수정!!
 });
